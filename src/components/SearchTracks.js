e3990f2c40ef42716ad306a8427a07c8ca0e1924
Dev3 <dev3@shayansolutions.com>
2020-02-21 15:46:30 +0500

    SearchTracks

--- src/components/SearchTracks.js
+++ src/components/SearchTracks.js
@@ -1,43 +1,51 @@
-import React, {Component} from "react";
-
-class SearchTracks extends Component {
-    state = {tracksQuery: ''};
-
-    updateTrackQuery = (e) => {
-        this.setState({tracksQuery: e.target.value});
-        this.searchTracks();
-    };
-
-    handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            this.searchTracks();
-        }
-    };
-    searchTracks = (e) => {
-        const {tracks} = this.props;
-        const filtertracks = tracks.filter(
-            track => track.name.toLowerCase().indexOf(this.state.tracksQuery.toLowerCase()) !== -1
-        );
-        this.props.searchResults(filtertracks);
-    };
-
-    render() {
-
-        return (
-            <div>
-                <input onChange={this.updateTrackQuery} onKeyPress={this.handleKeyPress}
-                       placeholder='Search for a track'/>
-                <button onClick={this.searchTracks}>Search</button>
-                <br/>
-                {this.filtertracks}
-                {this.state.tracksQuery}
-            </div>
-        )
-    }
-}
-
-export default SearchTracks;
-/*{
-                if (track.name.toLowerCase().includes(this.state.tracksQuery.toLowerCase()))
-                    return track;
-            }*/
+// import React, {Component} from "react";
+//
+// class SearchTracks extends Component {
+//     state = {tracksQuery: '', filtertracks: []};
+//
+//     updateTrackQuery = (e) => {
+//         this.setState({tracksQuery: e.target.value});
+//         this.searchTracks();
+//     };
+//
+//
+//     searchTracks = () => {
+//
+//         const {tracks} = this.props;
+//         let filtertracks = tracks.filter(
+//             track => track.name.toLowerCase().indexOf(this.state.tracksQuery.toLowerCase()) !== -1
+//         );
+//
+//         this.props.searchResults(filtertracks);
+//     };
+//
+//     render() {
+//         return (
+//             <div>
+//
+//                 <input onChange={this.updateTrackQuery}
+//                        placeholder='Search for a track'/>
+//                 <br/>
+//                 {this.state.tracksQuery === '' ? <div>{this.state.tracksQuery}</div> :
+//                     <div>{this.state.tracksQuery}</div>}
+//             </div>
+//         )
+//     }
+// }
+//
+// export default SearchTracks;
+//
+//
+// // <button onClick={this.searchTracks}>Search</button>
+//
+// // onKeyPress={this.handleKeyPress}
+// /*{
+//                 if (track.name.toLowerCase().includes(this.state.tracksQuery.toLowerCase()))
+//                     return track;
+//             }*/
+//
+// // handleKeyPress = (e) => {
+// //     if (e.key === 'Enter') {
+// //         this.searchTracks();
+// //     }
+// // };
--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,26 +1,38 @@
 import React, {Component} from 'react';
 import Pagination from "react-pagination-js";
 import "react-pagination-js/dist/styles.css";
-import SearchTracks from "./SearchTracks";
 
 class Tracks extends Component {
-
     state = {
         playing: false, audio: null, playingPreviewUrl: null,
         currentPage: 1,
         sizePerPage: 2,
+        tracksQuery: '',
         filterarray: [],
-
     };
 
-    searchResults = filterArray => {
-        this.setState({filterarray: filterArray});
+    updateTrackQuery = (e) => {
+        this.setState({currentPage: 1});
+        this.state.tracksQuery = e.target.value;
+        this.searchTracks();
+    };
+
+    searchTracks = () => {
+        const {tracks} = this.props;
+        let filtertracks = [];
+        if (this.state.tracksQuery === '')
+            this.setState({filterarray: []});
+        else {
+            filtertracks = tracks.filter(
+                track => track.name.toLowerCase().indexOf(this.state.tracksQuery.toLowerCase()) !== -1
+            );
+            this.setState({filterarray: filtertracks});
+        }
     };
 
     changeCurrentPage = numPage => {
         this.setState({currentPage: numPage});
     };
-
 
     playAudio = previewUrl => () => {
         const audio = new Audio(previewUrl);
@@ -51,26 +63,29 @@ class Tracks extends Component {
     };
 
     render() {
-       let {tracks} = this.props;
+        let {tracks} = this.props;
         if (this.state.filterarray.length) {
             tracks = this.state.filterarray;
         }
-        console.log(tracks);
-
         const endIndex = (this.state.sizePerPage * this.state.currentPage);
         const paginateTracks = tracks.slice(endIndex - this.state.sizePerPage, endIndex);
         return (
             <div>
                 <hr/>
-                <SearchTracks tracks={this.props.tracks} tracksState={this.state} searchResults={this.searchResults}/>
+                <input onChange={this.updateTrackQuery}
+                       placeholder='Search for a track'/>
+                <br/>
                 {
                     paginateTracks.map(track => {
                         const {id, name, album, preview_url} = track;
+                        {
+                            this.state.tracksQuery === '' ? <h2>Null</h2> : <p>{this.state.tracksQuery}</p>
+                        }
                         return (
-                            <div
-                                key={id}
-                                onClick={this.playAudio(preview_url)}
-                                className='track'>
+                            <div key={id}
+                                 onClick={this.playAudio(preview_url)}
+                                 className='track'>
+
                                 <img
                                     src={album.images[0].url}
                                     alt='track-image'
@@ -78,6 +93,7 @@ class Tracks extends Component {
                                 <p className='track-text'>{name}</p>
                                 <p className='track-icon'>{this.trackIcon(track)}</p>
                             </div>
+
                         )
                     })
                 }
@@ -87,6 +103,7 @@ class Tracks extends Component {
                     changeCurrentPage={this.changeCurrentPage}
                     sizePerPage={this.state.sizePerPage}/>
                 <h2>current Page:{this.state.currentPage}</h2>
+
             </div>
         )
 
@@ -96,13 +113,14 @@ class Tracks extends Component {
 export default Tracks;
 
 
-// const paginateTracks = tracks.slice(((this.state.sizePerPage * this.state.currentPage) - this.state.sizePerPage), this.state.sizePerPage * this.state.currentPage);
+// this.state.tracksQuery === '' ? <div>{this.state.tracksQuery}</div>
+// <div>{this.state.tracksQuery}</div>
 
+// const paginateTracks = tracks.slice(((this.state.sizePerPage * this.state.currentPage) - this.state.sizePerPage), this.state.sizePerPage * this.state.currentPage);
 
 // startIndex: (this.state.sizePerPage * this.state.currentPage) - this.state.sizePerPage,
 // endIndex: this.state.sizePerPage * this.state.currentPage,
 // console.log(this.state.startIndex,this.state.endIndex)
-
 
 // componentDidMount() {
 //     this.calculatePages();
@@ -124,3 +142,8 @@ export default Tracks;
 //     else
 //         this.setState({totalPages: ((this.props.tracks.length / this.state.sizePerPage) + 1)});
 // };
+
+// this.setState({tracksQuery: e.target.value});
+// console.log('event-target-value', e.target.value);
+// console.log("In Render",this.state.tracksQuery);
+// console.log("tracksquery",this.state.tracksQuery," Length",this.state.tracksQuery.length);
